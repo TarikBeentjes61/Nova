118494a21213199263f9121631281af380f12067
briandl2000 <brian@delangecastricum.nl>
2025-07-25 16:48:33 +0200

    added the start of the ui

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui
 from nova.ui import MainWindow
 
+def text_changed_callback(text):
+    suggestions = []
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        suggestions.append(f"{text}{letter}")
+    window.set_suggestions(suggestions)
+
 def main():
-    print("Nova is starting...")
+    global window
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.set_text_changed_callback(text_changed_callback)
+    window.show()
+
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
